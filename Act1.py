ecc0616976ac109f8a02cd4a0968d3bc9017085b
JuanTosetti <juanignaciotosetti7@gmail.com>
2026-03-16 00:49:43 -0300

    Resulucion punto 1

---
+++ Act1.py
@@ -0,0 +1,10 @@
+# Escribe un programa que solicite al usuario su año de nacimiento y muestre en qué
+# año cumplirá 18, 21 y 100 años.
+
+birth_year = int(input("Ingrese su año de nacimiento: "))
+print(f"""
+        Su año de nacimiento es {birth_year}
+        Cumplis 18 en {birth_year + 18}
+        Cumplis 21 en {birth_year + 21} 
+        Cumplis 100 en {birth_year + 100}
+        """)
